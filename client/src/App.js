0f115f538b847a91bc3e1adb36240ab3995e869b
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-04-12 20:15:12 -0500

    build: Set up scaffolding for alumni search, dashboard, and navbar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import AlumniSearch from './AlumniSearch';
+import Navbar from './Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is the app component</h1>
-    </div>
+    <Router className="App">
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Dashboard} />
+        <Route exact path='/alumnisearch'>
+          <AlumniSearch />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1> I am the Navbar </h1>
+    </div>
+  )
+}
+
+export default Navbar
