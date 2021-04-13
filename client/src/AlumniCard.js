dade77a8e013f6f57c92235205efbf487d0d781a
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-04-13 17:43:50 -0500

    feat: Add alumni card to capture API GET results.

--- client/src/AlumniCard.js
+++ client/src/AlumniCard.js
@@ -1,25 +1,26 @@
-import React from 'react'
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import React from "react"
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 
-const AlumniCard = () => {
+const AlumniCard = ({ name, link, avatar_url, email, location, hireable, company, bio, id}) => {
+  console.log(id);
+  console.log(avatar_url);
   return (
 
-    <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+    <Card style={{ width: "18rem" }}>
+    <Card.Img variant="top" src={avatar_url} />
     <Card.Body>
-      <Card.Title>Card Title</Card.Title>
+      <Card.Title>{id}</Card.Title>
       <Card.Text>
-        Some quick example text to build on the card title and make up the bulk of
-        the card's content.
+        {bio}
       </Card.Text>
     </Card.Body>
     <ListGroup className="list-group-flush">
-      <ListGroupItem>Cras justo odio</ListGroupItem>
-      <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-      <ListGroupItem>Vestibulum at eros</ListGroupItem>
+      <ListGroupItem>{email}</ListGroupItem>
+      <ListGroupItem>{company}</ListGroupItem>
+      <ListGroupItem>{hireable}</ListGroupItem>
     </ListGroup>
     <Card.Body>
-      <Card.Link href="#">Card Link</Card.Link>
+      <Card.Link href="#">Link to Github</Card.Link>
       <Card.Link href="#">Another Link</Card.Link>
     </Card.Body>
   </Card>
--- client/src/AlumniSearch.js
+++ client/src/AlumniSearch.js
@@ -1,21 +1,75 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import AlumniCard from './AlumniCard';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
 
 const AlumniSearch = () => {
+  const [searchString, setSearchString] = useState('');
   
-  
-  const searchAlumni = (input) => {
-    axios.get('https://api.github.com/users/' + input).then(res => {
-      console.log(res.data);
-
-    }).catch(err => console.log(err));
-  }
-
-  return (
-    <div>
-      <h1> Search Alumni </h1>
-    </div>
-  )
-}
-
-export default AlumniSearch
+  const [alumniData, setAlumniData] = useState({
+    name: '',
+    avatar_url: '',
+    email: '',
+    location: '',
+    hireable: '',
+    company: '',
+    bio: '',
+    id: '',
+  })
+  // Make API call to GET alumni data for a GitHub user. 
+  // If successful(200), console log the response(res) and capture it in state using "setAlumniData". (So "setAlumniData" is a value that holds that API res)
+  // If the call is unsuccessful, producing HTTP 500, 404, or any other error, capture and console log it.
+  const searchAlumni = (searchFor) =>
+  {
+    axios.get('https://api.github.com/users/' + searchFor)
+    .then(res =>
+      {
+        console.log(res);
+        setAlumniData({
+          name: res.data.name,
+          avatar_url: res.data.avatar_url,
+          email: res.data.email,
+          location: res.data.location,
+          hireable: res.data.hireable,
+          company: res.data.company,
+          bio: res.data.bio,
+          id: res.data.id,
+          link: res.data.link
+        })
+        
+      }).catch(err => console.log(err));
+    }
+// Input field for search bar. 
+// Note: In Bootstrap, mb-3 means "margin bottom, 3rem." 1 rem=the width of a capital "M" in HTML, which is usually 16px.
+// The onChange function receives the search string value captured by setSearchString. When a user clicks Search, the data is held in the searchString state. 
+// The searchString variable takes the initial value entered inside the parens. The onClick function triggers the searchAlumni GET function, passing in the value of the search string. So onChange takes in "e.target.value" and uses setSearchString as a variable to change and hold the searchString value.
+    return (
+      <div>
+      <InputGroup className="mb-3">
+      <FormControl
+      placeholder="Alumni username"
+      aria-label="Alumni username"
+      aria-describedby="basic-addon2"
+      onChange = {(e) => {setSearchString(e.target.value)}}
+      />
+      <InputGroup.Append>
+      <Button onClick={() => {searchAlumni(searchString)}} variant="outline-secondary">Search</Button>
+      </InputGroup.Append>
+      </InputGroup>
+      <AlumniCard 
+      name={alumniData.name}
+      avatar_url={alumniData.avatar_url}
+      email={alumniData.email}
+      location={alumniData.location}
+      hireable={alumniData.hireable}
+      company={alumniData.company}
+      bio={alumniData.bio}
+      id={alumniData.id}
+      link={alumniData.link}
+      />
+      </div>
+      )
+    }
+    // We use the AlumniCard, above, to pass through the response and set it equal to the data we want (e.g., name=alumniData.name). Each property on the card can be accessed by a function that grabs and holds their values. 
+    export default AlumniSearch
+    
