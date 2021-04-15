60c7a19bad5a23b4e545c21194f00c75e161db03
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-04-15 14:01:03 -0500

    style: On Dashboard, add image file to use for title. Add tag line. Center elements horizontally.

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import HeaderImage from './header.png';
+import HeaderImage from './Assets/header.png';
+import TitleImage from './title.png'
 import { Container } from 'react-bootstrap';
 
 const Dashboard = () => {
   return (
     <Container>
-      <img src={HeaderImage} alt='' />
+      <div>
+        <img src={HeaderImage} alt='Clip art rendering of a modern office.' className="header" />
+  </div>
+      <div className="text-center"><img src={TitleImage} alt='BootNetwork.' /></div>
       
+      <h2>There's strength in alumni.</h2>
     </Container>
   )
 }
