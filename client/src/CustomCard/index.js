64d1c0b4757cebba2c1040efd8f1b51ab2543ad6
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-04-14 21:34:20 -0500

    feat: Modified old card format with new card that has separate areas for image, text, and links.

--- client/src/CustomCard/index.js
+++ client/src/CustomCard/index.js
@@ -1,18 +1,19 @@
 import React from "react"
 import "./CustomCard.css";
 
+
 const CustomCard = ({ alumn }) =>
 {
   return (<React.Fragment Fragment>
     <div className="card">
       <div class="card-image"><img src={alumn.avatar_url} alt="BootNetwork Member" />
         <div className="card-text">
-      <h3>{alumn.name}</h3>
-      <h4>Email</h4>
+          <h2>{alumn.name}</h2>
+      <h3>Email</h3>
       <p>{alumn.email}</p>
-      <h4>Company</h4>
+      <h3>Company</h3>
       <p>{alumn.company}</p>
-      <h4>Bio</h4>
+      <h3>Bio</h3>
       <p>{alumn.bio}</p>
 </div>
       <div className="card-links">
