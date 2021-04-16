e45fcda8bf10bc280e20801e45bdf45deb5bf12a
Margaret Elson <margaret.a.elson@gmail.com>
2021-04-15 21:04:42 -0400

    githubuser.js and userindex.js done working on db file now

--- models/githubUser.js
+++ models/githubUser.js
@@ -5,11 +5,15 @@ const Schema = mongoose.Schema;
 const githubUserSchema = new Schema({
   avatar: String,
   name: String,
-  username: String,
+  username: {
+    type: String,
+    unique: true,
+  },
   bio: String,
+  company: String,
+  hireable: Boolean,
   portfolio: String,
   github: String,
-
 
 });
 
--- models/userindex.js
+++ models/userindex.js
@@ -1,1 +1,3 @@
-module.exports = {GithubUser: require "./githubUser.js"}
+module.exports = {
+    GithubUser: require ("./githubUser")
+};
