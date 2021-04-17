bafc9a35fdd1601d445307729898ec44601cf55e
Mateo Ferreira <matteo948@icloud.com>
2021-04-17 13:43:22 -0400

    Login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ContactWrapper from "../../../../../../../React/new apps/my-appa/src/conatctWrapper";
+import FormCard from "../../../../../../../React/new apps/my-appa/src/FormCard";
+import WelcomeCard from "../../../../../../../React/new apps/my-appa/src/WelcomeCard";
+import "./Login.css";
+
+const Login = () => {
+  return (
+<ContactWrapper>
+<WelcomeCard/>
+<FormCard/>
+
+</ContactWrapper>
+
+  );
+};
+
+export default Login;
