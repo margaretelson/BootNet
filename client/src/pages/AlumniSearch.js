a763c2a568156751c8d822572b8f238de369c5ed
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-04-17 12:22:11 -0500

    files: Add components and pages.

--- client/src/pages/AlumniSearch.js
+++ client/src/pages/AlumniSearch.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import CustomCard from "./CustomCard";
+import CustomCard from "../components/CustomCard";
 import {
   InputGroup,
   FormControl,
