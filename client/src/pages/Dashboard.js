8e9b40add0f0f347c830e7603466a9b67758ddce
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-04-17 21:05:19 -0500

    style: Remove title from mini-logo in nav bar. Add logo to Dashboard page. Center elelments. Adjust paths to get links working (more to be done on that). Add office image to navigation so that it will appear on every page, not just on the Dashboard page. On API.html page, change 'class' to 'className.'

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,23 +1,17 @@
 import React from "react";
-import HeaderImage from "../assets/header.png";
-import TitleImage from "../assets/title.png";
+import TitleImage from "../assets/BootNetwork-logo.png";
 import { Container } from "react-bootstrap";
 
 const Dashboard = () => {
   return (
     <Container>
-      <div>
-        <img
-          src={HeaderImage}
-          alt="Clip art rendering of a modern office"
-          className="header"
-        />
-      </div>
-      <div className="text-center">
+      
+      <div className="title-image">
         <img src={TitleImage} alt="BootNetwork" />
       </div>
-
-      <h2>There's strength in alumni.</h2>
+<div>
+        <h2>There's strength in alumni</h2>
+        </div>
     </Container>
   );
 };
