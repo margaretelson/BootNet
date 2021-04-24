842f57c209c342c29bb4a4e3b86b9feaab5e2cac
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-04-24 18:00:12 -0500

    fix: Resolve merge conflicts.

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,46 +1,23 @@
 import React from "react";
-import  { useState } from 'react';
-import { Container } from "react-bootstrap";
+import { useState } from "react";
+// import TitleImage from "../assets/BootNetwork-logo.png";
+// import { Container } from "react-bootstrap";
 import AllUsers from "../components/AllUsers";
 import Dashboardmain from "../components/DashboardALL";
-import UsersCard from "../components/UsersCard";
-
+// import UsersCard from "../components/UsersCard";
 
 /// get request to get the saved results
-
-
-
 
 //display results.map()
 //
 const Dashboard = () => {
-  const [results,setResults] = useState([])
+  const [results, setResults] = useState([]);
   return (
-<<<<<<< HEAD
-    <Container>
-      <div className="title-image">
-        <img src={TitleImage} alt="BootNetwork" />
-      </div>
-      {/* <div>
-        <h2>There's strength in alumni</h2>
-      </div> */}
-      {/* <LinkedinCompanyProfile
-        lang="en_US"
-        companyId={76820747}
-        format="inline" // Or "hover"
-        text="BootNetwork" // text to show in "hover" format */}
-      {/* /> */}
-    </Container>
-=======
     <Dashboardmain>
-      {results.map(result=>
-<AllUsers/>
-    
-      )}
-
+      {results.map((result) => (
+        <AllUsers />
+      ))}
     </Dashboardmain>
-
->>>>>>> 10b060f52a91bb439d508458b9cef35de9056fec
   );
 };
 
