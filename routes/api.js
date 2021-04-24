1ae066d43630468dffd8a9acd362a8546be2cfcd
LindseyBowen <lindseyjbowen@gmail.com>
2021-04-23 22:31:55 -0400

    have login and signup page showing up. You can sign up but can't login.

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,83 @@
 const router = require ("express").Router()
 const axios = require ("axios")
+const passport = require ("passport")
+const User = require ("../models/user")
 
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.getUserByUsername(username, function(err, user){
+      if(err) throw err;
+      if(!user){
+        return done(null, false, {message: 'Unknown User'});
+      }
+      User.comparePassword(password, user.password, function(err, isMatch){
+        if(err) throw err;
+     	if(isMatch){
+     	  return done(null, user);
+     	} else {
+     	  return done(null, false, {message: 'Invalid password'});
+     	}
+     });
+   });
+  }
+));
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
 router.get("/api/github/:user", function(req,res){
     axios.get( "https://api.github.com/users/" + req.params.user).then(function(results){
         res.json(results.data)
     })
 })
+
+// Register User
+router.post('/api/register', function(req, res){
+    var password = req.body.password;
+    var password2 = req.body.password2;
+  
+    if (password == password2){
+      var newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+      });
+  
+      User.createUser(newUser, function(err, user){
+        if(err) throw err;
+        res.send(user).end()
+      });
+    } else{
+      res.status(500).send("{errors: \"Passwords don't match\"}").end()
+    }
+  });
+// Endpoint to login
+router.post('/api/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    res.send(req.user);
+  }
+);
+
+// Endpoint to get current user
+router.get('/api/user', function(req, res){
+  res.send(req.user);
+})
+
+
+// Endpoint to logout
+router.get('/api/logout', function(req, res){
+  req.logout();
+  res.send(null)
+});
+
 
 module.exports=router
 
