5324c447a4bf0855469a4bb27e99fe8953d5ba55
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-04-24 16:01:20 -0500

    fix: Fix typo, remove tagline, and add BootNetwork image on 'components/FormCard/index.js'. Make header image and gradient background mobile responsive. Fix merge conflicts.

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,7 @@
 import React from "react";
 import TitleImage from "../assets/BootNetwork-logo.png";
 import { Container } from "react-bootstrap";
-<<<<<<< HEAD
-=======
-import AllUsers from "../components/AllUsers";
->>>>>>> d617afd0d3f2604863206cdaf603fd11e6701a20
-// import { LinkedinCompanyProfile } from "react-social-plugins";
+// import AllUsers from "../components/AllUsers";
 
 const Dashboard = () => {
   return (
@@ -13,26 +9,16 @@ const Dashboard = () => {
       <div className="title-image">
         <img src={TitleImage} alt="BootNetwork" />
       </div>
-      <div>
+      {/* <div>
         <h2>There's strength in alumni</h2>
-      </div>
+      </div> */}
       {/* <LinkedinCompanyProfile
         lang="en_US"
         companyId={76820747}
         format="inline" // Or "hover"
         text="BootNetwork" // text to show in "hover" format */}
       {/* /> */}
-<<<<<<< HEAD
     </Container>
-=======
-      <AllUsers 
-      />
-       
-    )
-    </Container>
-
-    
->>>>>>> d617afd0d3f2604863206cdaf603fd11e6701a20
   );
 };
 
