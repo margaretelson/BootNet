8ec98d8c055654a9c13fc7fe0ac90c477b522b9c
Margaret Elson <margaret.a.elson@gmail.com>
2021-04-25 21:39:56 -0400

    deploying

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
-import HeaderImage from "../../assets/header.png";
+// import HeaderImage from "../.."
 import { Link } from "react-router-dom";
-import BootLogo from "../../assets/logo-sans-title.png";
+// import BootLogo from "../../assets/logo-sans-title.png";
 import { Container } from "react-bootstrap";
 
 const Navigation = () => {
@@ -9,7 +9,7 @@ const Navigation = () => {
     <Container>
       <div className="nav-container">
         <Link to="/">
-          <img style={{ width: "50px" }} src={BootLogo} alt="Cloud logo" />
+          <img style={{ width: "50px" }}  alt="Cloud logo" />
         </Link>
         <Link className="link" to="/dashboard">
           Dashboard
@@ -27,7 +27,7 @@ const Navigation = () => {
 
       <div>
         <img
-          src={HeaderImage}
+          
           alt="Clip-art rendering of a modern office"
           className="header"
         />
